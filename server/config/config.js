a8d89bb63e9496f8ded0ad5ed2ad97aa73c43819
Nico <pablolitardo@gmail.com>
2020-11-28 15:11:37 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://nico:qDDR33u7CSF7YdrI@cluster0.ju8mt.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
