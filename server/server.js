b42a66c4bcd9e4b7c436b318cec32e6a21ddad9a
Nico <pablolitardo@gmail.com>
2020-11-29 17:54:21 -0300

    Google Sing-In

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config.js');
 const express = require('express');
 const mongoose = require('./config/newmongoose');
+const path = require('path');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -11,10 +12,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+
+
 // Configuracion global de rutas
 app.use(require('./routes/index'));
-
-
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if (err) throw err;
